b1853f227b8cbf33e24fee84ea63b5f197832e27
Piotr Gogola <gogola.piotr@interia.pl>
2021-01-06 23:51:01 +0100

    Create unlimited amount of new requests

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,6 +1,9 @@
-use single_threaded_server::SingleThreadServer;
+use multi_threaded_server::MultiThreadServer;
+// use single_threaded_server::SingleThreadServer;
 
 fn main() {
-    let single_threaded_server = SingleThreadServer::new();
-    single_threaded_server.start_listening();
+    // let single_threaded_server = SingleThreadServer::new();
+    // single_threaded_server.start_listening();
+    let multi_threaded_server = MultiThreadServer::new();
+    multi_threaded_server.start_listening();
 }
