d6849a813df878f300d151aa109c4c54443ccc1b
Piotr Gogola <gogola.piotr@interia.pl>
2021-01-06 23:03:32 +0100

    Send 200 OK response

--- single-threaded-server/src/lib.rs
+++ single-threaded-server/src/lib.rs
@@ -21,6 +21,9 @@ impl SingleThreadServer {
     fn handle_connection(&self, mut stream: TcpStream) {
         let mut buffer = [0; 1024];
         stream.read(&mut buffer).unwrap();
-        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+        let response = "HTTP/1.1 200 OK\r\n\r\n";
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
